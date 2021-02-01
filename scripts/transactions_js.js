0c09a916c8bb9efc1011131eb1199111b5c7b20e
Jack <suzijie860706@gmail.com>
2021-02-01 09:34:49 +0800

    更新

--- scripts/transactions_js.js
+++ scripts/transactions_js.js
@@ -2,7 +2,7 @@
 var button1 = document.getElementById('button1');
 button1.addEventListener('click', (e) => {
     var textArea1 = document.getElementById('text1');
-    var data = textArea1.value.split(/<summary>|<\/summary>|\n/);
+    var data = textArea1.value.split(/\/\/\/ <summary>|<\/summary>|\r\n/);
     //過濾掉空值項目
     data.forEach(element => {
         if(element.trim() == '') 
@@ -11,16 +11,20 @@ button1.addEventListener('click', (e) => {
             data.splice(nummber,1);
         } 
     });
+    data.forEach(element => {
+        console.log(element);
+    });
     //提取類型、變數名稱
     var data2 = [];
-    for (let x = 0; x < data.length; x+=3) {
-        data2.push(data[x + 1])
+    for (let x = 0; x < data.length; x+=2) {
+        data2.push(data[x])
     }
     //第二次分類 將類型、變數
     var data3 = [];
     data2.forEach(element => {
         data3.push(element.split(' '));
     });
+    
     //過濾掉空值項目
     data3.forEach(element => {
         element.forEach(element_2 => {
@@ -31,6 +35,7 @@ button1.addEventListener('click', (e) => {
             } 
         });
     });
+    
     var textArea2 = '/// <summary>' + '\n';
     data3.forEach(element => {
         textArea2 += '/// <para>' + element[0] + ' ' + element[1] + ' ' + element[2] + '</para>\n';
@@ -43,12 +48,11 @@ button1.addEventListener('click', (e) => {
 
 
 
-
 //#region 新增中文註解
 var button1 = document.getElementById('button2');
 button1.addEventListener('click', (e) => {
     var textArea1 = document.getElementById('text1');
-    var data = textArea1.value.split(/public|{|\n/);
+    var data = textArea1.value.split(/public|{|\r\n/);
     //過濾掉空值項目
     data.forEach(element => {
         if(element.trim() == '') 
@@ -57,6 +61,7 @@ button1.addEventListener('click', (e) => {
             data.splice(nummber,1);
         } 
     });
+    
     //提取類型、變數名稱
     var data2 = [];
     for (let x = 0; x < data.length; x+=2) {
@@ -77,10 +82,13 @@ button1.addEventListener('click', (e) => {
             } 
         });
     });
+    data3.forEach(element => {
+        console.log(element);    
+    });
     var textArea2 = '';
     data3.forEach(element => {
-        textArea2 += '/// <summary>' + element[0] + " " + element[1] + ' ' + '</summary>' + '\n';
-        textArea2 += 'public ' + element[0] + ' ' + element[1] + ' { get; set; }' + '\n';
+        textArea2 += '/// <summary>' + element[0] + " " + element[1] + ' ' + '</summary>' + '\r\n';
+        textArea2 += 'public ' + element[0] + ' ' + element[1] + ' { get; set; }' + '\r\n';
     });
     document.getElementById('text2').value = textArea2;
 })
